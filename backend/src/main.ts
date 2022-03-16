9d3077f0a93466fe3ddce37d64b0e74a1d598b39
plx-edu <plx-edu@hotmail.com>
2022-03-16 10:00:36 +0100

    small feedback update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 3001;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(port, () => console.log(`:: Listening on port ${port}`));
 }
 bootstrap();
