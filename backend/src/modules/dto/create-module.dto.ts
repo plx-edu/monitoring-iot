b73f590951740cf3efb28abe6ab88a8c117f6098
plx-edu <plx-edu@hotmail.com>
2022-03-16 11:18:48 +0100

    added dto

--- backend/src/modules/dto/create-module.dto.ts
+++ backend/src/modules/dto/create-module.dto.ts
@@ -1,1 +1,8 @@
-export class CreateModuleDto {}
+export class CreateModuleDto {
+  id: number;
+  name: string;
+  type: string;
+  location: string;
+  current_value: number;
+  current_state: boolean;
+}
