8ee1aef9bda2bcd3f35c41dc897ca346faf57cd7
plx-edu <plx-edu@hotmail.com>
2022-03-21 00:39:05 +0100

    failed tw-elements import attempt

--- frontend/src/components/Module/ModuleDetail.tsx
+++ frontend/src/components/Module/ModuleDetail.tsx
@@ -51,54 +51,107 @@ export default function ModuleDetail() {
 		return <section>Error: {JSON.stringify(error)}</section>;
 	} else {
 		return (
-			<section className="flex">
-				{/* Logs Section */}
-				<div className="flex">
-					<ul>
-						{item?.data_log.reverse().map((data) => (
-							<li key={data.id}>
-								<p>{data.measured}</p>
-								<p>{formatDate(data.time)}</p>
-							</li>
-						))}
-					</ul>
-
-					<ul>
-						{item?.state_log.reverse().map((state) => (
-							<li key={state.id}>
-								<p>State: {state.state ? "true" : "false"}</p>
-								<p>Set by user: {state.user_set ? "true" : "false"}</p>
-								<p>{formatDate(state.time)}</p>
-							</li>
-						))}
-					</ul>
-				</div>
-
+			<section className="flex justify-between">
 				{/* Module section */}
-				<section className="flex flex-col gap-5">
-					<Module module={item} />
-					<button
-						type="button"
-						onClick={handleToggle}
-						className={
-							"flex flex-col w-40 rounded overflow-hidden " + (item?.current_state ? "bg-red-500" : "bg-emerald-400")
-						}
-					>
-						{item?.current_state ? "Deactivate" : "Activate"}
-					</button>
-					<button
-						type="button"
-						onClick={handleDelete}
-						className="flex flex-col w-40 rounded overflow-hidden bg-red-600"
-					>
-						<div className="flex h-full justify-around items-center">
-							<AiOutlineWarning />
-							<p>Delete</p>
-							<AiOutlineWarning />
+				{/* <section className="flex flex-col gap-5 bg-zinc-800"> */}
+				<section className="relative h-screen w-60 bg-zinc-500">
+					<section className="flex flex-col gap-5 items-center sticky top-20">
+						<Module module={item} />
+						<button
+							type="button"
+							onClick={handleToggle}
+							className={
+								"flex flex-col w-40 rounded overflow-hidden " + (item?.current_state ? "bg-red-500" : "bg-emerald-400")
+							}
+						>
+							{item?.current_state ? "Deactivate" : "Activate"}
+						</button>
+						<button
+							type="button"
+							onClick={handleDelete}
+							className="flex flex-col w-40 rounded overflow-hidden bg-red-600"
+						>
+							<div className="flex h-full justify-around items-center">
+								<AiOutlineWarning />
+								<p>Delete</p>
+								<AiOutlineWarning />
+							</div>
+						</button>
+					</section>
+				</section>
+
+				{/* Logs Section */}
+				<section className="flex flex-col w-full items-center gap-5 pt-2">
+					{/* Graph */}
+					<section className="flex flex-col max-h-64 w-5/6 overflow-hidden rounded border border-zinc-800">
+						<div className="flex flex-row  bg-zinc-800 text-white">
+							<h1 className="font-bold w-full text-center">Graph...</h1>
 						</div>
-					</button>
+						<div className="flex">
+							{/* <p className="w-full text-center">"n/a"</p> */}
+							<canvas className="p-10" id="chartLine"></canvas>
+						</div>
+					</section>
+
+					{/* Data Logs */}
+					<section className="flex flex-col max-h-64 w-5/6 overflow-hidden rounded border border-zinc-800">
+						<div className="flex flex-row  bg-zinc-800 text-white">
+							<h1 className="font-bold w-full text-center">Value</h1>
+							<h1 className="font-bold w-full text-center">Time</h1>
+						</div>
+						<section className="overflow-y-auto">
+							{item?.data_log.reverse().map((data) => (
+								<div key={data.id} className="flex flex-row">
+									<p className="w-full text-center">{data.measured ? data.measured : "n/a"}</p>
+									<p className="w-full text-center">{formatDate(data.time)}</p>
+								</div>
+							))}
+						</section>
+					</section>
+
+					{/* State Logs */}
+					<section className="flex flex-col max-h-64 w-5/6 overflow-hidden rounded border border-zinc-800">
+						<div className="flex flex-row  bg-zinc-800 text-white">
+							<h1 className="font-bold w-full text-center">State</h1>
+							<h1 className="font-bold w-full text-center">Origin</h1>
+							<h1 className="font-bold w-full text-center">Time</h1>
+						</div>
+						<section className="overflow-y-auto">
+							{item?.state_log.reverse().map((state) => (
+								<div key={state.id} className="flex flex-row">
+									<p className="w-full text-center">{state.state ? "Active" : "Inactive"}</p>
+									<p className="w-full text-center">{state.user_set ? "User" : "Unknown"}</p>
+									<p className="w-full text-center">{formatDate(state.time)}</p>
+								</div>
+							))}
+						</section>
+					</section>
 				</section>
 			</section>
 		);
 	}
+}
+
+function setupChart() {
+	const labels = ["January", "February", "March", "April", "May", "June"];
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: "My First dataset",
+				backgroundColor: "hsl(252, 82.9%, 67.8%)",
+				borderColor: "hsl(252, 82.9%, 67.8%)",
+				data: [0, 10, 5, 2, 20, 30, 45],
+			},
+		],
+	};
+
+	const configLineChart = {
+		type: "line",
+		data,
+		options: {},
+	};
+
+	// let chartLine = new Chart(document.getElementById("chartLine"), configLineChart);
+	// let chartLine = (document.getElementById("chartLine"), configLineChart);
 }
