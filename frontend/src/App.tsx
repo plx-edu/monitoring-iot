381fdf498f76a75e0f6c954a8b03860976774a09
plx-edu <plx-edu@hotmail.com>
2022-03-22 00:13:59 +0100

    moved script to Dashboard component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,84 +5,18 @@ import DashBoard from "./components/DashBoard/DashBoard";
 import ModuleDetail from "./components/Module/ModuleDetail";
 import ModuleForm from "./components/Module/ModuleForm";
 import TypeForm from "./components/DashBoard/TypeForm";
-import {apiRequest, apiResource, formatDate, getRandNb, msToMins, startInterval} from "./utilities/methods";
-import {createContext, useEffect, useState} from "react";
-import {typeModule} from "./utilities/types";
-
-// export const ModulesContext = createContext<typeModule[]>([[], () => {}]);
-export const ModulesContext = createContext<any>([[], () => {}]);
 
 function App() {
-	const [modulesList, setModulesList] = useState<typeModule[]>([]);
-	const [startScript, setStartScript] = useState(false);
-
-	useEffect(() => {
-		console.log(":: Hello Module ::");
-
-		fetch(apiResource("modules"))
-			.then((res) => res.json())
-			.then((result) => {
-				setModulesList(result);
-				setTimeout(() => {
-					setStartScript(result.length > 0);
-				}, 5 * 1000);
-			});
-	}, []);
-
-	// *Automatic* Script
-	useEffect(() => {
-		if (modulesList.length <= 0) return;
-		console.log(":: Starting Script ::");
-
-		setInterval(() => {
-			const moduleToUpdate = modulesList[getRandNb(0, modulesList.length - 1)];
-			console.log("#", moduleToUpdate);
-
-			// If module is not active, don't do anything :/
-			if (!moduleToUpdate.current_state) return;
-
-			const newData = {
-				state: {
-					current_state: false,
-					user_set: false,
-				},
-				measurement: {
-					current_state: true,
-					current_value: getRandNb(0, 50),
-				},
-			};
-
-			// More chances of updating measurement than failing module state
-			const dataToUpdate = getRandNb(1, 100) <= 10 ? newData.state : newData.measurement;
-			console.log("::", newData);
-
-			fetch(apiResource("modules", moduleToUpdate.id), apiRequest("patch", dataToUpdate))
-				.then((res) => res.json())
-				.then((result) => {
-					console.log(formatDate(new Date(Date.now())), result);
-
-					setModulesList(
-						modulesList.map((x) => {
-							if (x.id === result.id) return result;
-							return x;
-						}),
-					);
-				});
-		}, msToMins(10));
-	}, [startScript]);
-
 	return (
 		<section className="App h-screen w-screen bg-slate-300 overflow-hidden">
 			<Layout>
-				<ModulesContext.Provider value={[modulesList, setModulesList]}>
-					<Routes>
-						<Route path="/" element={<DashBoard />} />
+				<Routes>
+					<Route path="/" element={<DashBoard />} />
 
-						<Route path="/module/" element={<ModuleForm />} />
-						<Route path="/module/:id" element={<ModuleDetail />} />
-						<Route path="/type/" element={<TypeForm />} />
-					</Routes>
-				</ModulesContext.Provider>
+					<Route path="/module/" element={<ModuleForm />} />
+					<Route path="/module/:id" element={<ModuleDetail />} />
+					<Route path="/type/" element={<TypeForm />} />
+				</Routes>
 			</Layout>
 		</section>
 	);
--- frontend/src/components/DashBoard/DashBoard.tsx
+++ frontend/src/components/DashBoard/DashBoard.tsx
@@ -1,24 +1,74 @@
-import React, {useContext, useEffect, useState} from "react";
-import {ModulesContext} from "../../App";
-import {apiResource} from "../../utilities/methods";
+import React, {useEffect, useState} from "react";
+import {apiRequest, apiResource, formatDate, getRandNb, msToMins} from "../../utilities/methods";
 import {typeModule} from "../../utilities/types";
 import Module from "../Module/Module";
 
+// Script interval in minutes
+const minInterval = 30;
+
 export default function DashBoard() {
-	// const [items, setItems] = useState<typeModule[]>([]);
-	const [items, setItems] = useContext(ModulesContext);
+	const [items, setItems] = useState<typeModule[]>([]);
+	const [startScript, setStartScript] = useState(false);
 
-	// useEffect(() => {
-	// 	// console.log("dashboard re");
+	useEffect(() => {
+		// console.log(":: Modules Dashboard ::");
 
-	// 	fetch(apiResource("modules"))
-	// 		.then((res) => res.json())
-	// 		.then((result) => setItems(result));
-	// }, []);
+		fetch(apiResource("modules"))
+			.then((res) => res.json())
+			.then((result) => {
+				setItems(result);
+
+				// Wait before starting script
+				setTimeout(() => {
+					setStartScript(result.length > 0);
+				}, 15 * 1000);
+			});
+	}, []);
+
+	// *Automatic* Script
+	useEffect(() => {
+		if (items.length <= 0) return;
+		// console.log(":: Starting Script ::");
+
+		setInterval(() => {
+			const moduleToUpdate = items[getRandNb(0, items.length - 1)];
+			// console.log("#", moduleToUpdate);
+
+			// If module is not active, don't do anything :/
+			if (!moduleToUpdate.current_state) return;
+
+			const newData = {
+				state: {
+					current_state: false,
+					user_set: false,
+				},
+				measurement: {
+					current_value: getRandNb(0, 50),
+				},
+			};
+
+			// More chances of updating measurement than failing module state
+			const dataToUpdate = getRandNb(1, 100) <= 10 ? newData.state : newData.measurement;
+			// console.log("::", newData);
+
+			fetch(apiResource("modules", moduleToUpdate.id), apiRequest("patch", dataToUpdate))
+				.then((res) => res.json())
+				.then((result) => {
+					console.log(formatDate(new Date(Date.now())), result);
+
+					setItems(
+						items.map((x) => {
+							if (x.id === result.id) return result;
+							return x;
+						}),
+					);
+				});
+		}, msToMins(minInterval));
+	}, [startScript]);
 
 	return (
 		<section className="flex flex-col h-full">
-			Modules:
+			Dashboard:
 			<section className=" flex flex-row flex-wrap justify-center m-1 gap-1">
 				{items.map((module: typeModule) => (
 					<Module key={module.id} module={module} />
