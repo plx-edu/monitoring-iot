f4ace9ed8c8f87c86742d54663e76b1a3fe211d3
plx-edu <plx-edu@hotmail.com>
2022-03-23 09:07:16 +0100

    'optimised' dashboard component

--- frontend/src/components/DashBoard/DashBoard.tsx
+++ frontend/src/components/DashBoard/DashBoard.tsx
@@ -1,56 +1,51 @@
 import React, {useEffect, useState} from "react";
-import {apiRequest, apiResource, formatDate, getRandNb, msToMins} from "../../utilities/methods";
+import {apiResource, msToMins} from "../../utilities/methods";
 import {typeModule} from "../../utilities/types";
 import Module from "../Module/Module";
 
 // Script interval in minutes
 // msToMins(minInterval)
 const minInterval = 0.2;
+let startScript = false;
 
 export default function DashBoard() {
 	const [items, setItems] = useState<typeModule[]>([]);
-	const [startScript, setStartScript] = useState(false);
-	const [changeInterval, setChangeInterval] = useState(false);
+	const [checkChanges, setCheckChanges] = useState(false);
 
 	useEffect(() => {
 		console.log(":: Modules Dashboard ::");
+
+		// Wait before starting script
+		const allowScript = setTimeout(() => {
+			startScript = true;
+		}, 15 * 1000);
+
 		const intervalId = setInterval(() => {
-			setChangeInterval((ci) => !ci);
+			setCheckChanges((ci) => !ci);
 		}, msToMins(minInterval));
 
-		fetch(apiResource("modules"))
-			.then((res) => res.json())
-			.then((result) => {
-				setItems(result);
+		fetchModules();
 
-				// Wait before starting script
-				setTimeout(() => {
-					setStartScript(result.length > 0);
-				}, 15 * 1000);
-			});
-
-		return () => clearInterval(intervalId);
+		return () => {
+			clearTimeout(allowScript);
+			clearInterval(intervalId);
+		};
 	}, []);
 
-	/*
-	// *Automatic* Script
 	useEffect(() => {
-	}, [startScript]);
-*/
+		if (!startScript) return;
+		fetchModules();
+	}, [checkChanges]);
 
-	useEffect(() => {
-		console.log(changeInterval ? "console" : "logged", items.length);
-
-		// return () => {
-		// 	second
-		// }
-	}, [changeInterval]);
-
-	// function random
+	function fetchModules() {
+		fetch(apiResource("modules"))
+			.then((res) => res.json())
+			.then((result) => setItems(result));
+	}
 
 	return (
 		<section className="flex flex-col h-full">
-			Dashboard:{changeInterval ? "on" : "off"}
+			Dashboard:{checkChanges ? "on" : "off"}
 			<section className=" flex flex-row flex-wrap justify-center m-1 gap-1">
 				{items.map((module: typeModule) => (
 					<Module key={module.id} module={module} />
